f72208e3647e60b1c7ac035ee76365f8b5131aaa
aiviaghost <smurfplatta@gmail.com>
2022-09-10 01:00:26 +0200

    Finish implementing GF_256_Polynomial

--- four/aes.py
+++ four/aes.py
@@ -3,8 +3,6 @@ from polynomial import GF_256_Polynomial
 
 class AES:
 
-    POLY_MOD = GF_256_Polynomial([1, 0, 0, 0, 1, 1, 0, 1, 1])
-
     @staticmethod
     def sbox(r, c):
         return SBOX[16 * r + c]
@@ -14,17 +12,17 @@ class AES:
         return INV_SBOX[16 * r + c]
 
     @staticmethod
-    def RotWord(w):
-        assert len(w) == 4, f"RotWord expects input to be 4 bytes!, got {len(w)}"
+    def rot_word(w):
+        assert len(w) == 4, f"rot_word expects input to be 4 bytes, got {len(w)} bytes!"
         return w[1 : ] + w[0 : 1]
     
     @staticmethod
-    def SubWord(w):
-        assert len(w) == 4, f"SubWord expects input to be 4 bytes!, got {len(w)}"
+    def sub_word(w):
+        assert len(w) == 4, f"sub_word expects input to be 4 bytes, got {len(w)} bytes!"
         return bytes(AES.sbox(lower, upper) for lower, upper in map(lambda b: ((b & 0b11110000) >> 4, b & 0b1111), w))
     
     @staticmethod
-    def Rcon(i):
+    def rcon(i):
         x = GF_256_Polynomial([1, 0])
-        r = GF_256_Polynomial.pow_mod(x, i - 1, AES.POLY_MOD).to_num()
+        r = GF_256_Polynomial.pow(x, (i - 1) % 255).to_num()
         return bytes((r, 0, 0, 0))
--- four/polynomial.py
+++ four/polynomial.py
@@ -4,46 +4,64 @@ class GF_256_Polynomial:
         return [int(i) for i in "".join(map(str, coeffs)).rstrip("0")]
 
     def __init__(self, coeffs, rev = True):
-        self.coeffs = self.__trim_zeros(coeffs[::-1] if rev else coeffs)
-        self.deg = len(self.coeffs) - 1
+        self.__coeffs = self.__trim_zeros(coeffs[::-1] if rev else coeffs)
+        self.deg = len(self.__coeffs) - 1
+
+    def coeffs(self):
+        return self.__coeffs.copy()
 
     @staticmethod
     def from_num(num):
         return GF_256_Polynomial([int(i) for i in bin(num)[2 : ]])
     
     def to_num(self):
-        return int("".join(map(str, self.coeffs[::-1])), 2)
+        return int("".join(map(str, self.__coeffs[::-1])), 2)
 
     def __mul__(self, other):
         res = [0] * (self.deg + other.deg + 1)
-        for i, c1 in enumerate(self.coeffs):
-            for j, c2 in enumerate(other.coeffs):
+        for i, c1 in enumerate(self.__coeffs):
+            for j, c2 in enumerate(other.coeffs()):
                 res[i + j] ^= c1 * c2
         return GF_256_Polynomial(res, rev = False)
 
     def __add__(self, other):
         if self.deg >= other.deg:
-            p1 = self.coeffs
-            p2 = other.coeffs
+            p1 = self.coeffs()
+            p2 = other.coeffs()
         else:
-            p1 = other.coeffs
-            p2 = self.coeffs
+            p1 = other.coeffs()
+            p2 = self.coeffs()
         for i, c in enumerate(p2):
             p1[i] ^= c
         return GF_256_Polynomial(p1, rev = False)
-    
+
     def __sub__(self, other):
         return self + other
 
+    def shift(self, steps):
+        return GF_256_Polynomial([0] * steps + self.__coeffs, rev = False)
+
     def __mod__(self, other):
-        pass
+        N = GF_256_Polynomial(self.coeffs(), rev = False)
+        D = GF_256_Polynomial(other.coeffs(), rev = False)
+        q = [0] * (N.deg + 1)
+        while N.deg >= other.deg:
+            deg_diff = N.deg - D.deg
+            d = D.shift(deg_diff)
+            q[deg_diff] = N.coeffs()[N.deg] * d.coeffs()[d.deg]
+            d = d * GF_256_Polynomial([q[deg_diff]])
+            N = N - d
+        return N
 
     @staticmethod
-    def pow_mod(base, exp, mod):
+    def pow(base, exp):
         res = GF_256_Polynomial([1])
         while exp > 0:
             if (exp & 1) == 1:
-                base = (base * base) % mod
-            res = (res * base) % mod
+                res = (res * base) % GF_256_Polynomial.REDUCTION_POLY
+            base = (base * base) % GF_256_Polynomial.REDUCTION_POLY
             exp >>= 1
         return res
+
+# Ugly hack to have static member of same type as the class it is a member of
+GF_256_Polynomial.REDUCTION_POLY = GF_256_Polynomial([1, 0, 0, 0, 1, 1, 0, 1, 1])
